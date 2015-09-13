6c08f7f44ae13fecbed69483935ca5339890d1c6
Parijat Mazumdar <mazumdarparijat@gmail.com>
2015-09-12 19:57:33 -0500

    removed unused variable serverAddress + code comments

--- src/main/java/cs425/mp1/regexMatcher.java
+++ src/main/java/cs425/mp1/regexMatcher.java
@@ -4,27 +4,46 @@ import java.io.*;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-import org.apache.commons.cli.ParseException;
-
+/**
+ * Class for matching regex with lines of a log file.
+ * It takes query pattern and log file as input. It
+ * also takes an output stream as input in which it
+ * puts the matching lines. This class supports all
+ * grep matching control options.
+ */
 public class regexMatcher {
 	private grepQuery pattern;
 	private final PrintWriter out;
 	private final String logFile;
-	private final String requestServerAddress;
 
-	private regexMatcher(grepQuery pattern, String fname, PrintWriter output, String serverAddress)
+    /** Constructor
+     * @param pattern pattern string with options
+     * @param fname log filename
+     * @param output output stream in which matching lines are put
+     * @throws FileNotFoundException
+     */
+	private regexMatcher(grepQuery pattern, String fname, PrintWriter output)
 			throws FileNotFoundException {
 		this.pattern = pattern;
 		out = output;
 		logFile = fname;
-		this.requestServerAddress = serverAddress;
 	}
 
-	static regexMatcher getNewInstance(grepQuery pattern, String fname, PrintWriter output, String sourceID)
+    /** Get new instance
+     * @param pattern pattern string with options
+     * @param fname log filename
+     * @param output output stream to put matching lines
+     * @return object of this class
+     * @throws FileNotFoundException
+     */
+	static regexMatcher getNewInstance(grepQuery pattern, String fname, PrintWriter output)
 			throws FileNotFoundException {
-		return new regexMatcher(pattern, fname, output, sourceID);
+		return new regexMatcher(pattern, fname, output);
 	}
 
+    /** Matches regex with lines and puts matching lines in outputstream
+     * @throws IOException
+     */
 	public void printMatchingLines() throws IOException {
 		String line;
 		// System.out.println("[RegexMatcher] pattern : "+pattern);
